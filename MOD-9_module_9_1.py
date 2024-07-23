56880d6e50afc7c2c40f6e87758fad373eb1da12
Selemin Yuriy <71262890+yuriselemin@users.noreply.github.com>
2024-07-23 19:21:13 +0300

    Add files via upload

---
+++ MOD-9_module_9_1.py
@@ -0,0 +1,14 @@
+
+
+# Домашнее задание по теме "Введение в функциональное программирование"
+
+
+def apply_all_func(int_list, *functions):
+    results = {}
+    for func in functions:
+        results[func.__name__] = func(int_list)
+    return results
+
+
+print(apply_all_func([6, 20, 15, 9], max, min))
+print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
